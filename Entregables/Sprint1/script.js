cebb9c39dc587f810c224a83abab989d233e034b
Andres Palma <palma.castaneda015@gmail.com>
2021-07-17 10:58:31 -0500

    Actualizacion:17 julio 2021

---
+++ Entregables/Sprint1/script.js
@@ -0,0 +1,30 @@
+
+    
+    function calculo() {
+        let cantidadComp = document.getElementById('cantidad-comp').value;
+        let totalSinDescuento = cantidadComp*820000
+        let totalCompra = 0
+        
+        
+        if (totalSinDescuento >= 1640000 && totalSinDescuento <= 3280000){
+            totalCompra = totalSinDescuento*0.85;
+
+        
+        }
+        else if (totalSinDescuento <= 6560000 && totalSinDescuento > 3280000) {
+            totalCompra = totalSinDescuento*0.75;
+        }
+        else if (totalSinDescuento > 6560000 && totalSinDescuento<=9840000) {
+            totalCompra = totalSinDescuento*0.65;
+        }
+
+        else {
+            totalCompra = totalSinDescuento;
+            document.getElementById('alerta').value = "El cliente no recibe descuento"
+        }
+        document.getElementById('valor-compra').value = totalSinDescuento
+        document.getElementById('valor-descuento').value = totalSinDescuento-totalCompra
+        document.getElementById('total').value = totalCompra
+
+    }
+    
