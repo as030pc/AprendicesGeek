7abcc8670dfd30212536623cea03f7ab44833c3b
Andres Palma <palma.castaneda015@gmail.com>
2021-08-13 11:00:28 -0500

    Modulo3

--- Campo_entrenamiento/Modulo3/Tarea1IMC/script.js
+++ Campo_entrenamiento/Modulo3/Tarea1IMC/script.js
@@ -4,6 +4,11 @@ let estado = document.getElementById('indiceMasa')
 let arreglo =  [];
 let imc = 1;
 let estadoPersona
+let nivel;
+
+addEventListener('DOMContentLoaded', () => {
+    localStorage.clear()
+})
 
 boton.addEventListener('click', evento => {
     evento.preventDefault(); //Se controla el evento
@@ -11,7 +16,7 @@ boton.addEventListener('click', evento => {
     let edad = document.getElementById('edad').value
     let peso = Number(document.getElementById('peso').value)
     let estatura = Number(document.getElementById('estatura').value)
-    console.log(sexo , edad ,peso, estatura)
+    //console.log(sexo , edad ,peso, estatura)
     calcularIMC(estatura, peso, sexo,edad);
     
 })
@@ -20,19 +25,22 @@ boton.addEventListener('click', evento => {
 
 const calcularIMC = (estatura, peso, sexo, edad) => {
     imc = peso/(Math.pow(estatura,2))
-
+    nivel = (imc*100)/40
     if (imc < 18.5) {
         estadoPersona = "Bajo Peso"
-
+        
     } else if (imc <= 18.5 && imc >= 24.9) {
         estadoPersona = "Saludable"
-
+        
     } else if (imc <= 25 && imc >= 29.9) {
         estadoPersona = "Con Sobrepeso"
+        
     } else if (imc <= 30 && imc >= 39.9) {
         estadoPersona = "Obeso"
+        
     } else  {
         estadoPersona = "Obesidad Extrema o de alto Riesgo"
+        
     } 
     mostrar()
     creacionRegistro(sexo, edad, peso, estatura, imc, estadoPersona)
@@ -44,7 +52,12 @@ const calcularIMC = (estatura, peso, sexo, edad) => {
 
 
 const mostrar = () => {
-    estado.innerHTML = `<p> El indice de masa corporal del usuario es ${imc.toFixed(2)} <br> Su estado de salud es ${estadoPersona}</p>`
+    estado.innerHTML = `<p> El indice de masa corporal del usuario es ${imc.toFixed(2)} <br> Su estado de salud es ${estadoPersona}</p>
+    <form name="formulario" method="post" action="/send.php" id = "medidor">
+    <div id = "escala"> <h5> Bajo peso </h5>   <meter min="0" max="100" low="25" high="75"
+    optimum="100" value="${nivel}">  <h5> Obesidad extrema </h5> </div>
+  </form>
+  `
 
 }
 
@@ -59,13 +72,44 @@ const creacionRegistro = (sexo, edad, peso, estatura, imc, estado) => {
 
     }
     arreglo.push(registro); //Permite guardar la informacion del objeto en un arreglo
-    console.log(arreglo);
+    //console.log(arreglo);
 
 }
 
 const guardarLocalStorage = () => {
     localStorage.setItem('datos', JSON.stringify(arreglo))
+    let datosEstadistica = localStorage.getItem("datos");
+    
+    //console.log(datosEstadistica)
+    let obj = JSON.parse(datosEstadistica);
+    //console.log(obj)
+    obj.forEach(element => {
+        const{edad,sexo, estatura, imc, estado } = element;
+        console.log(edad, sexo, estado, imc, estatura)
+        // if (sexo == "Mujer") {
+        //     let contador = contador +1
+        // }
+        // console.log(contador)
+    })
+
+    
+    
+
+
+
+    
+
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
